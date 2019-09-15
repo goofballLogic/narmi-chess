dbfeec5e43f292522e449d6971587d8bad9edd38
Andrew Gibson <andrew@goofballLogic.com>
2019-09-15 19:03:17 +0100

    broken stopping point

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,84 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Piece {
+    Empty = 0,
+    Pawn = 1,
+    Rook = 2,
+    Knight = 3,
+    Bishop = 4,
+    Queen = 5,
+    King = 6,
+    IsWhite = 8
+}
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum GameState {
+    NotStarted = 0,
+    Started = 1,
+    Stalemate = 2,
+    WhiteResigned = 3,
+    BlackResigned = 4,
+    WhiteCheckmate = 5,
+    BlackCheckmate = 6
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Position {
+    rank: u8,
+    file: u8,
+    piece: Piece
+}
+
+#[wasm_bindgen]
+pub struct Game {
+    positions: Vec<Position>,
+    is_white_move: bool,
+    state: GameState
+}
+
+#[wasm_bindgen]
+impl Game {
+
+    pub fn new() {
+        Game {
+            is_white_move: true,
+            state: GameState::NotStarted,
+            positions: [ Position {
+                rank: 0,
+                file: 0,
+                piece: Piece::Rook
+            } ].to_vec()
+        }
+    }
+
+    pub fn positions(&self) -> *const Position {
+        self.positions.as_ptr()
+    }
+    pub fn is_white_move(&self) -> bool {
+        self.is_white_move
+    }
+    pub fn state(&self) -> GameState {
+        self.state
+    }
+}
+
+#[wasm_bindgen]
 extern {
     fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn console_log(s: &str);
+    #[wasm_bindgen(js_namespace = Date, js_name = now)]
+    fn date_now() -> f64;
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    alert(&format!("Hello {} from narmi-chess!", name));
+
+    console_log(&format_args!("Hello {} at {}", name, date_now()).to_string());
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,6 @@
 import * as wasm from "narmi-chess";
 
 wasm.greet("Imran");
+console.log("WASM", wasm);
+var game = new wasm.Game();
+console.log(game);
